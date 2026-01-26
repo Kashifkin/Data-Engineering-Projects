f426bf9f8d71192a58fad20a4050c18bedfc035b
Kashif  Khan <111867054+Kashifkin@users.noreply.github.com>
2026-01-26 11:03:33 +0500

    Delete ETL Pipeline directory

--- ETL Pipeline/Project_Pipeline/transformations/Loading_File/Loading_File_1.py
+++
@@ -1,9 +0,0 @@
-import dlt 
-
-@dlt.table(
-    name = "Gold_Sales_1"
-)
-
-def Gold_Sales_1():
-  df = spark.read.table("silver_sales")
-  return df
--- ETL Pipeline/Project_Pipeline/transformations/Loading_File/Loading_File_2.py
+++
@@ -1,9 +0,0 @@
-import dlt 
-
-@dlt.table(
-    name = "Gold_Sales_2"
-)
-
-def Gold_Sales_2():
-  df = spark.read.table("silver_sales_2")
-  return df
